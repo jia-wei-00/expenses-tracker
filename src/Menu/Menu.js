12164992e012593700f422267740dc44fcef0e6b
leong_jia_wei <wwwbingo6@gmail.com>
2022-09-18 03:08:42 +0800

    Update Menu.js

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Menu.css";
 import PaidIcon from "@mui/icons-material/Paid";
 import HistoryIcon from "@mui/icons-material/History";
@@ -11,6 +11,22 @@ import { connect } from "react-redux";
 
 function Menu(props) {
   const [active, setActive] = useState("home");
+  const [show, setShow] = useState(true);
+  const [height, setHeight] = useState();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerHeight);
+
+      if (height < 600) {
+        setShow(false);
+      } else if (height >= 600) {
+        setShow(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, [window.innerHeight]);
 
   return (
     <>
@@ -23,46 +39,48 @@ function Menu(props) {
           <LogoutIcon />
         </IconButton>
       </div>
-      <div className="navigation">
-        <ul>
-          <li
-            className={active === "home" ? "list active" : "list"}
-            onClick={() => setActive("home")}
-          >
-            <Link to="/home">
-              <span className="icon">
-                <PaidIcon />
-              </span>
-              <span className="text">Tracker</span>
-            </Link>
-          </li>
+      {show && (
+        <div className="navigation">
+          <ul>
+            <li
+              className={active === "home" ? "list active" : "list"}
+              onClick={() => setActive("home")}
+            >
+              <Link to="/home">
+                <span className="icon">
+                  <PaidIcon />
+                </span>
+                <span className="text">Tracker</span>
+              </Link>
+            </li>
 
-          <li
-            className={active === "settings" ? "list active" : "list"}
-            onClick={() => setActive("settings")}
-          >
-            <Link to="/history">
-              <span className="icon">
-                <HistoryIcon />
-              </span>
-              <span className="text">History</span>
-            </Link>
-          </li>
+            <li
+              className={active === "settings" ? "list active" : "list"}
+              onClick={() => setActive("settings")}
+            >
+              <Link to="/history">
+                <span className="icon">
+                  <HistoryIcon />
+                </span>
+                <span className="text">History</span>
+              </Link>
+            </li>
 
-          <li
-            className={active === "todo" ? "list active" : "list"}
-            onClick={() => setActive("todo")}
-          >
-            <Link to="login">
-              <span className="icon">
-                <TopicIcon />
-              </span>
-              <span className="text">Todo</span>
-            </Link>
-          </li>
-          <div className="indicator"></div>
-        </ul>
-      </div>
+            <li
+              className={active === "todo" ? "list active" : "list"}
+              onClick={() => setActive("todo")}
+            >
+              <Link to="login">
+                <span className="icon">
+                  <TopicIcon />
+                </span>
+                <span className="text">Todo</span>
+              </Link>
+            </li>
+            <div className="indicator"></div>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
