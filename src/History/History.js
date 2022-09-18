1c34dee7a64937af49e418fdfb9417fa28a8f8ee
leong_jia_wei <wwwbingo6@gmail.com>
2022-09-19 01:43:32 +0800

    update

--- src/History/History.js
+++ src/History/History.js
@@ -1,17 +1,45 @@
 import React, { useState, useEffect } from "react";
 import "./History.css";
 import { getHistoryAPI } from "../actions";
+import firebase from "firebase/compat/app";
 import { connect } from "react-redux";
 import Moment from "moment";
-import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import drilldown from "highcharts-drilldown";
+drilldown(Highcharts);
 
 const History = (props) => {
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(
+    Moment(firebase.firestore.Timestamp.now().toDate()).format("MMMM YYYY")
+  );
   const [balance, setBalance] = useState(0);
   const [expense, setExpense] = useState(0);
   const [income, setIncome] = useState(0);
+  const [showChart, setShowChart] = useState(0);
+  const [alignment, setAlignment] = useState("expense");
 
   let tmpDate = Moment(date).format("MMMM YYYY");
+  let data = [
+    {
+      name: "Chrome",
+      y: 61.04,
+    },
+    {
+      name: "Safari",
+      y: 9.47,
+    },
+    {
+      name: "Edge",
+      y: 9.32,
+    },
+    {
+      name: "Firefox",
+      y: 8.15,
+    },
+  ];
 
   const calculateAmount = async () => {
     let tmpincome = 0;
@@ -39,9 +67,142 @@ const History = (props) => {
     await props.getRecord(payload);
   };
 
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  const options = {
+    chart: {
+      type: "pie",
+    },
+    title: {
+      text: "Browser market shares. January, 2022",
+    },
+    subtitle: {
+      text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>',
+    },
+
+    accessibility: {
+      announceNewData: {
+        enabled: true,
+      },
+      point: {
+        valueSuffix: "%",
+      },
+    },
+
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: true,
+          format: "{point.name}: {point.y:.1f}%",
+        },
+      },
+    },
+
+    tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat:
+        '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>',
+    },
+
+    series: [
+      {
+        name: "Browsers",
+        colorByPoint: true,
+        data: data,
+      },
+    ],
+    // drilldown: {
+    //   series: [
+    //     {
+    //       name: "Chrome",
+    //       id: "Chrome",
+    //       data: [
+    //         ["v97.0", 36.89],
+    //         ["v96.0", 18.16],
+    //         ["v95.0", 0.54],
+    //         ["v94.0", 0.7],
+    //         ["v93.0", 0.8],
+    //         ["v92.0", 0.41],
+    //         ["v91.0", 0.31],
+    //         ["v90.0", 0.13],
+    //         ["v89.0", 0.14],
+    //         ["v88.0", 0.1],
+    //         ["v87.0", 0.35],
+    //         ["v86.0", 0.17],
+    //         ["v85.0", 0.18],
+    //         ["v84.0", 0.17],
+    //         ["v83.0", 0.21],
+    //         ["v81.0", 0.1],
+    //         ["v80.0", 0.16],
+    //         ["v79.0", 0.43],
+    //         ["v78.0", 0.11],
+    //         ["v76.0", 0.16],
+    //         ["v75.0", 0.15],
+    //         ["v72.0", 0.14],
+    //         ["v70.0", 0.11],
+    //         ["v69.0", 0.13],
+    //         ["v56.0", 0.12],
+    //         ["v49.0", 0.17],
+    //       ],
+    //     },
+    //     {
+    //       name: "Safari",
+    //       id: "Safari",
+    //       data: [
+    //         ["v15.3", 0.1],
+    //         ["v15.2", 2.01],
+    //         ["v15.1", 2.29],
+    //         ["v15.0", 0.49],
+    //         ["v14.1", 2.48],
+    //         ["v14.0", 0.64],
+    //         ["v13.1", 1.17],
+    //         ["v13.0", 0.13],
+    //         ["v12.1", 0.16],
+    //       ],
+    //     },
+    //     {
+    //       name: "Edge",
+    //       id: "Edge",
+    //       data: [
+    //         ["v97", 6.62],
+    //         ["v96", 2.55],
+    //         ["v95", 0.15],
+    //       ],
+    //     },
+    //     {
+    //       name: "Firefox",
+    //       id: "Firefox",
+    //       data: [
+    //         ["v96.0", 4.17],
+    //         ["v95.0", 3.33],
+    //         ["v94.0", 0.11],
+    //         ["v91.0", 0.23],
+    //         ["v78.0", 0.16],
+    //         ["v52.0", 0.15],
+    //       ],
+    //     },
+    //   ],
+    // },
+  };
+
   useEffect(() => {
     fetchRecord();
     calculateAmount();
+    setShowChart(false);
+
+    const fetchData = async () => {
+      await props.history.forEach((record, key) => {
+        data.push({
+          name: props.history[key].name,
+          y: parseFloat(props.history[key].amount),
+        });
+      });
+      console.log(data);
+      setShowChart(true);
+    };
+    fetchData().catch(console.error);
   }, [props.history.length, date]);
 
   return (
@@ -58,17 +219,36 @@ const History = (props) => {
       <div className="history__container">
         <h3>Balance ({tmpDate})</h3>
         <span>RM{balance}</span>
-        <div className="record__box">
-          <div className="amount__income">
-            <h4>INCOME</h4>
-            <p>RM{income}</p>
-          </div>
-          <div className="amount__expense">
-            <h4>EXPENSE</h4>
-            <p>RM{expense}</p>
-          </div>
-        </div>
-        <h3 className="history__title">History</h3>
+
+        <ToggleButtonGroup
+          color="primary"
+          value={alignment}
+          exclusive
+          onChange={handleChange}
+          aria-label="Platform"
+          style={{ width: "100%", marginTop: "20px" }}
+        >
+          <ToggleButton
+            style={{ border: "1px solid rgba(255,255,255,0.1)" }}
+            className="history__expense"
+            value="expense"
+          >
+            <h4>EXPENSE</h4>(<p>RM{expense}</p>)
+          </ToggleButton>
+          <ToggleButton
+            style={{ border: "1px solid rgba(255,255,255,0.1)" }}
+            className="history__income"
+            value="income"
+          >
+            <h4>INCOME</h4>(<p>RM{income}</p>)
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        {showChart && (
+          <HighchartsReact highcharts={Highcharts} options={options} />
+        )}
+
+        <h3 className="history__page__history">History</h3>
 
         <div className="history__record">
           {props.history.length > 0 &&
